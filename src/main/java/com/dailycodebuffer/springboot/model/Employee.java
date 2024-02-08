73d5e694a3709e9054f06eed8f8371224308a4dd
Fazeel Usmani <fazeelusmani18@gmail.com>
2024-02-08 23:46:29 +0530

    API Versioning

--- src/main/java/com/dailycodebuffer/springboot/model/Employee.java
+++ src/main/java/com/dailycodebuffer/springboot/model/Employee.java
@@ -1,13 +1,17 @@
 package com.dailycodebuffer.springboot.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+//@JsonIgnoreProperties({"department"})
 public class Employee {
     private String employeeId;
     private String firstName;
     private String lastName;
     private String emailId;
+//    @JsonIgnore
     private String department;
 
 }
